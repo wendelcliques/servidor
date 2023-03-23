b583e8fcf3389b7bfda72d8c39db1cc029642698
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2023-03-22 22:51:59 -0300

    servidor

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mercadopago = require('mercadopago');
+const cors = require("cors");
+
+const config = require('./config/index.json');
+
+
+
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+
+mercadopago.configure({
+    access_token: config.token
+  });
+
+
+//Routes
+server.post('/', (req, res) => {
+    let preference = {
+        items: [
+          {
+            title: 'Meu produto',
+            unit_price: 100,
+            quantity: 1,
+          }
+        ],
+
+        payment_methods: {
+            installments: 1
+        }
+      };
+      
+      mercadopago.preferences.create(preference)
+      .then(function(data){
+
+        res.send(JSON.stringify(data.response.sandbox_init_point));
+      // Este valor substituirá a string "<%= global.id %>" no seu HTML
+        global.id = data.response.body.id;
+      }).catch(function(error){
+        console.log(error);
+      });
+});
+
+
+
+
+
+let port = process.env.PORT || 3000;
+server.listen(port, (req, res) => {
+    console.log('Servidor rodando');
+})
